5147a9067b39ee4423d17be96434c6505a772dd9
parthR1910 <121542134+parthR1910@users.noreply.github.com>
2023-08-28 15:36:38 +0530

    tic toc toe game completed

--- lib/ui/home/controller/home_controller.dart
+++ lib/ui/home/controller/home_controller.dart
@@ -1,102 +1,190 @@
+import 'dart:async';
+import 'package:confetti/confetti.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final homeController = ChangeNotifierProvider((ref) => HomeController());
+// final maxSecondProvider = Provider<int>((ref) => 30);
 
-class HomeController extends ChangeNotifier{
-  List<String> displayXO= ['','','','','','','','',''];
+class HomeController extends ChangeNotifier {
+  List<String> displayXO = ['', '', '', '', '', '', '', '', ''];
   bool isOTurn = true;
   String resultDeclaration = "";
-
+  int attempt = 0;
   int oWins = 0;
   int xWins = 0;
   int filledBox = 0;
   bool winnerFound = false;
 
-  onTap(int index){
-    if(isOTurn && displayXO[index] == ''){
-      displayXO[index] = 'O';
-      filledBox++;
-    }else if(!isOTurn && displayXO[index] == ''){
-      displayXO[index] = 'X';
-      filledBox++;
+  onTap(int index) {
+    final isRunning = timer == null ? false : timer!.isActive;
+    if (isRunning) {
+      if (isOTurn && displayXO[index] == '') {
+        displayXO[index] = 'O';
+        filledBox++;
+      } else if (!isOTurn && displayXO[index] == '') {
+        displayXO[index] = 'X';
+        filledBox++;
+      }
+      isOTurn = !isOTurn;
+      checkWinner();
     }
-    isOTurn = !isOTurn;
-    checkWinner();
   }
 
+  ///this maxIndex for color change
+  final confettiController = ConfettiController();
 
-  void checkWinner(){
+  void confettiEffect(bool isConfettiPlay) {
+    if (isConfettiPlay) {
+      confettiController.play();
+    } else if (!isConfettiPlay) {
+      confettiController.stop();
+    }
+    // isConfettiPlay = !isConfettiPlay;
+    notifyListeners();
+  }
 
+  List<int> maxIndex = [];
+
+  void checkWinner() {
     ///1st row
-    if(displayXO[0] == displayXO[1] && displayXO[0] == displayXO[2] && displayXO[0] != ''){
+    if (displayXO[0] == displayXO[1] &&
+        displayXO[0] == displayXO[2] &&
+        displayXO[0] != '') {
       resultDeclaration = "Player ${displayXO[0]} Wins!";
+      maxIndex.addAll([0, 1, 2]);
       updateScore(displayXO[0]);
+      stopAndResetTimer();
+      confettiEffect(true);
     }
+
     ///2nd row
-    if(displayXO[3] == displayXO[4] && displayXO[3] == displayXO[5] && displayXO[3] != ''){
+    if (displayXO[3] == displayXO[4] &&
+        displayXO[3] == displayXO[5] &&
+        displayXO[3] != '') {
       resultDeclaration = "Player ${displayXO[3]} Wins!";
+      maxIndex.addAll([3, 4, 5]);
       updateScore(displayXO[3]);
+      stopAndResetTimer();
+      confettiEffect(true);
     }
+
     ///3rd row
-    if(displayXO[6] == displayXO[7] && displayXO[6] == displayXO[8] && displayXO[6] != ''){
+    if (displayXO[6] == displayXO[7] &&
+        displayXO[6] == displayXO[8] &&
+        displayXO[6] != '') {
       resultDeclaration = "Player ${displayXO[6]} Wins!";
+      maxIndex.addAll([6, 7, 8]);
       updateScore(displayXO[6]);
+      stopAndResetTimer();
+      confettiEffect(true);
     }
 
     ///1st Column
-    if(displayXO[0] == displayXO[3] && displayXO[0] == displayXO[6] && displayXO[0] != ''){
+    if (displayXO[0] == displayXO[3] &&
+        displayXO[0] == displayXO[6] &&
+        displayXO[0] != '') {
       resultDeclaration = "Player ${displayXO[0]} Wins!";
+      maxIndex.addAll([0, 3, 6]);
       updateScore(displayXO[0]);
+      stopAndResetTimer();
+      confettiEffect(true);
     }
-    ///2nd Column
-    if(displayXO[1] == displayXO[4] && displayXO[1] == displayXO[7] && displayXO[1] != ''){
-      resultDeclaration = "Player ${displayXO[1]} Wins!";
-      updateScore(displayXO[1]);
-    }
-    ///3rd Column
-    if(displayXO[2] == displayXO[5] && displayXO[2] == displayXO[8] && displayXO[2] != ''){
-      resultDeclaration = "Player ${displayXO[2]} Wins!";
-      updateScore(displayXO[2]);
 
+    ///2nd Column
+    if (displayXO[1] == displayXO[4] &&
+        displayXO[1] == displayXO[7] &&
+        displayXO[1] != '') {
+      resultDeclaration = "Player ${displayXO[1]} Wins!";
+      maxIndex.addAll([1, 4, 7]);
+      updateScore(displayXO[1]);
+      stopAndResetTimer();
+      confettiEffect(true);
+    }
+
+    ///3rd Column
+    if (displayXO[2] == displayXO[5] &&
+        displayXO[2] == displayXO[8] &&
+        displayXO[2] != '') {
+      resultDeclaration = "Player ${displayXO[2]} Wins!";
+      maxIndex.addAll([2, 5, 8]);
+      updateScore(displayXO[2]);
+      stopAndResetTimer();
+      confettiEffect(true);
     }
 
     ///1st cross
-    if(displayXO[0] == displayXO[4] && displayXO[0] == displayXO[8] && displayXO[0] != ''){
+    if (displayXO[0] == displayXO[4] &&
+        displayXO[0] == displayXO[8] &&
+        displayXO[0] != '') {
       resultDeclaration = "Player ${displayXO[0]} Wins!";
+      maxIndex.addAll([0, 4, 8]);
       updateScore(displayXO[0]);
-
+      stopAndResetTimer();
+      confettiEffect(true);
     }
+
     ///2nd row
-    if(displayXO[2] == displayXO[4] && displayXO[2] == displayXO[6] && displayXO[2] != '') {
+    if (displayXO[2] == displayXO[4] &&
+        displayXO[2] == displayXO[6] &&
+        displayXO[2] != '') {
       resultDeclaration = "Player ${displayXO[2]} Wins!";
+      maxIndex.addAll([2, 4, 6]);
       updateScore(displayXO[2]);
-
+      stopAndResetTimer();
+      confettiEffect(true);
     }
-    if(!winnerFound && filledBox == 9){
+    if (!winnerFound && filledBox == 9) {
       resultDeclaration = "NoBody Wins!";
+      stopAndResetTimer();
     }
     notifyListeners();
   }
 
-
-  updateScore (String index){
-    if(index== 'O'){
+  updateScore(String index) {
+    if (index == 'O') {
       oWins++;
-    }else if(index == 'X'){
+    } else if (index == 'X') {
       xWins++;
     }
     winnerFound = true;
     notifyListeners();
   }
 
-  clearData(){
-    for(int i = 0;i<9;i++){
-      displayXO[i]= '';
+  Timer? timer;
+
+  ///you can't access the const value directly through the dependency injection
+  static const maxSecond = 30;
+
+  int second = maxSecond;
+
+  void timerFunction() {
+    timer = Timer.periodic(const Duration(seconds: 1), (_) {
+      if (second > 0) {
+        second--;
+        notifyListeners();
+      } else {
+        stopAndResetTimer();
+        notifyListeners();
+      }
+    });
+    notifyListeners();
+  }
+
+  void stopAndResetTimer() {
+    second = maxSecond;
+    timer?.cancel();
+    notifyListeners();
+  }
+
+  clearData() {
+    for (int i = 0; i < 9; i++) {
+      displayXO[i] = '';
     }
+    isOTurn = true;
     resultDeclaration = "";
     winnerFound = false;
-    filledBox =0;
+    filledBox = 0;
     notifyListeners();
   }
 }
--- lib/ui/home/home.dart
+++ lib/ui/home/home.dart
@@ -1,3 +1,6 @@
+import 'dart:math' show pi;
+
+import 'package:confetti/confetti.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -6,128 +9,190 @@ import 'package:tic_toc_toe/ui/home/controller/home_controller.dart';
 import 'package:tic_toc_toe/ui/utils/common_device_configuration.dart';
 import 'package:tic_toc_toe/ui/utils/theme/app_color.dart';
 
-class Home extends StatelessWidget {
+class Home extends ConsumerWidget {
   const Home({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context,WidgetRef ref) {
     mobileConfiguration(context);
-    return Scaffold(
-      backgroundColor: AppColors.red,
-      body: _bodyWidget,
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        Scaffold(
+          backgroundColor: AppColors.red,
+          body: _bodyWidget(ref),
+        ),
+        ConfettiWidget(confettiController: ref.watch(homeController).confettiController,
+        colors: [AppColors.yellow,AppColors.red,AppColors.blue,AppColors.white],
+          blastDirection: - pi /2,
+          emissionFrequency: 0.1,
+          gravity: 0.1,
+        ),
+      ],
     );
   }
 
-  get _bodyWidget => Consumer(
-        builder: (context, ref, child) {
-          final homeWatch = ref.watch(homeController);
-          return Padding(
-            padding: const EdgeInsets.all(20),
-            child: Column(
-              children: [
-                Expanded(
-                  flex: 1,
-                  child: Padding(
-                    padding:  EdgeInsets.only(top:15.h),
-                    child: Row(
-                      children: [
-                        Expanded(
-                            child: Column(
-                          children: [
-                             Text("Player O",style: GoogleFonts.coiny(
-                                textStyle:
-                                TextStyle(fontSize: 20.sp, color: Colors.white)),
-                            ),
-                            SizedBox(
-                              height: 10.h,
-                            ),
-                            Text("Wins ${homeWatch.oWins}",style: GoogleFonts.coiny(
-                                textStyle:
-                                TextStyle(fontSize: 14.sp, color: Colors.white)),
-                            ),
-                          ],
-                        )),
-                        Expanded(
-                          flex: 1,
-                            child: Column(
-                          children: [
-                             Text("Player X",style: GoogleFonts.coiny(
-                                textStyle:
-                                TextStyle(fontSize: 20.sp, color: Colors.white)),
-                            ),
-                            SizedBox(
-                              height: 10.h,
-                            ),
-                            Text("Wins ${homeWatch.xWins}",style: GoogleFonts.coiny(
-                                textStyle:
-                                TextStyle(fontSize: 14.sp, color: Colors.white)),
-                            )
-                          ],
-                        ))
-                      ],
+
+  Widget _bodyWidget(WidgetRef ref) {
+    final homeWatch = ref.watch(homeController);
+    final isRunning =
+    homeWatch.timer == null ? false : homeWatch.timer!.isActive;
+    return Padding(
+      padding: const EdgeInsets.all(20),
+      child: Column(
+        children: [
+          Expanded(
+            flex: 1,
+            child: Padding(
+              padding: EdgeInsets.only(top: 15.h),
+              child: Row(
+                children: [
+                  Expanded(
+                      child: Column(
+                        children: [
+                          Text(
+                            "Player O",
+                            style: GoogleFonts.coiny(
+                                textStyle: TextStyle(
+                                    fontSize: 20.sp, color: Colors.white)),
+                          ),
+                          SizedBox(
+                            height: 10.h,
+                          ),
+                          Text(
+                            "Wins ${homeWatch.oWins}",
+                            style: GoogleFonts.coiny(
+                                textStyle: TextStyle(
+                                    fontSize: 14.sp, color: Colors.white)),
+                          ),
+                        ],
+                      )),
+                  Expanded(
+                      flex: 1,
+                      child: Column(
+                        children: [
+                          Text(
+                            "Player X",
+                            style: GoogleFonts.coiny(
+                                textStyle: TextStyle(
+                                    fontSize: 20.sp,
+                                    color: Colors.white)),
+                          ),
+                          SizedBox(
+                            height: 10.h,
+                          ),
+                          Text(
+                            "Wins ${homeWatch.xWins}",
+                            style: GoogleFonts.coiny(
+                                textStyle: TextStyle(
+                                    fontSize: 14.sp,
+                                    color: Colors.white)),
+                          )
+                        ],
+                      ))
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+              flex: 3,
+              child: GridView.builder(
+                itemCount: homeWatch.displayXO.length,
+                gridDelegate:
+                const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3),
+                itemBuilder: (context, index) => GestureDetector(
+                  onTap: () {
+                    homeWatch.onTap(index);
+                  },
+                  child: Container(
+                    alignment: Alignment.center,
+                    height: 80.h,
+                    width: 80.w,
+                    decoration: BoxDecoration(
+                        color: homeWatch.maxIndex.contains(index)
+                            ? AppColors.blue
+                            : AppColors.yellow,
+                        borderRadius: BorderRadius.circular(15.r),
+                        border:
+                        Border.all(color: AppColors.red, width: 5)),
+                    child: Text(
+                      homeWatch.displayXO[index],
+                      style: GoogleFonts.coiny(
+                          textStyle: TextStyle(
+                              fontSize: 60.sp, color: AppColors.red)),
                     ),
                   ),
                 ),
-                Expanded(
-                    flex: 3,
-                    child: GridView.builder(
-                      itemCount: homeWatch.displayXO.length,
-                      gridDelegate:
-                          const SliverGridDelegateWithFixedCrossAxisCount(
-                              crossAxisCount: 3),
-                      itemBuilder: (context, index) => GestureDetector(
-                        onTap: () {
-                          homeWatch.onTap(index);
-                        },
-                        child: Container(
-                          alignment: Alignment.center,
-                          height: 80.h,
-                          width: 80.w,
-                          decoration: BoxDecoration(
-                              color: AppColors.yellow,
-                              borderRadius: BorderRadius.circular(15.r),
-                              border:
-                                  Border.all(color: AppColors.red, width: 5)),
+              )),
+          Expanded(
+              flex: 2,
+              child: Column(
+                children: [
+                  Text(
+                    homeWatch.resultDeclaration,
+                    style: GoogleFonts.coiny(
+                        textStyle: TextStyle(
+                            fontSize: 30.sp, color: AppColors.white)),
+                  ),
+                  SizedBox(
+                    height: 10.h,
+                  ),
+                  isRunning
+                      ? SizedBox(
+                    height: 100.h,
+                    width: 100.h,
+                    child: Stack(
+                      fit: StackFit.expand,
+                      children: [
+                        CircularProgressIndicator(
+                          value: 1 -
+                              homeWatch.second /
+                                  HomeController.maxSecond,
+                          valueColor: const AlwaysStoppedAnimation(
+                              Colors.white),
+                          strokeWidth: 8,
+                          backgroundColor: AppColors.blue,
+                        ),
+                        Center(
                           child: Text(
-                            homeWatch.displayXO[index],
+                            "${homeWatch.second}",
                             style: GoogleFonts.coiny(
                                 textStyle: TextStyle(
-                                    fontSize: 60.sp, color: AppColors.red)),
+                                    fontSize: 50.sp,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold)),
                           ),
-                        ),
-                      ),
-                    )),
-                Expanded(
-                    flex: 2,
-                    child: Column(
-                      children: [
-                        Text(
-                          homeWatch.resultDeclaration,
-                          style: GoogleFonts.coiny(
-                              textStyle: TextStyle(
-                                  fontSize: 30.sp, color: AppColors.white)),
-                        ),
-                        SizedBox(height: 10.h,),
-                        ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                                backgroundColor: AppColors.white,
-                              padding: EdgeInsets.symmetric(horizontal: 30.w,vertical: 16.h)
-                            ),
-                            onPressed: () {
-                              homeWatch.clearData();
-                            },
-                            child: Text(
-                              "Play Again",
-                              textAlign: TextAlign.center,
-                              style: GoogleFonts.coiny(
-                                  textStyle:
-                                  TextStyle(fontSize: 20.sp, color: Colors.black)),
-                            ))
+                        )
                       ],
-                    )),
-              ],
-            ),
-          );
-        },
-      );
+                    ),
+                  )
+                      : ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: AppColors.white,
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 30.w, vertical: 16.h)),
+                      onPressed: () {
+                        homeWatch.timerFunction();
+                        homeWatch.clearData();
+                        homeWatch.attempt++;
+                        homeWatch.maxIndex = [];
+                        homeWatch.confettiEffect(false);
+                      },
+                      child: Text(
+                        homeWatch.attempt == 0
+                            ? "Start"
+                            : "Play Again",
+                        textAlign: TextAlign.center,
+                        style: GoogleFonts.coiny(
+                            textStyle: TextStyle(
+                                fontSize: 20.sp,
+                                color: Colors.black)),
+                      ))
+                ],
+              )),
+        ],
+      ),
+    );
+  }
 }
